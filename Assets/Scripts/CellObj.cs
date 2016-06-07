160300b712d2bce86f0038a3c7967a8809e67d01
unstablesun <unstablesun@gmail.com>
2016-06-07 14:47:14 -0700

    Initial Commit

---
+++ Assets/Scripts/CellObj.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellObj : MonoBehaviour {
+
+
+
+	public GameObject lifeSprite = null;
+
+	public struct VirtualSlice
+	{
+
+		public bool IsAlive { get; set; }  
+		public float r, g, b;
+
+	}
+
+	public VirtualSlice[] _slices = new VirtualSlice[2];
+
+	public int _currentSlice = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	//
+	//}
+
+
+	//SLICE ACCESS
+	public void SetCurrentSlice(int s)
+	{
+		_currentSlice = s;
+	}
+
+
+	public void ToggleCurrentSlice()
+	{
+		if (_currentSlice == 0) {
+			_currentSlice = 1;
+		} else {
+			_currentSlice = 0;
+		}
+	}
+
+
+	public void SetCurrentSliceData(bool isAlive, float r, float g, float b)
+	{
+		_slices[_currentSlice].IsAlive = isAlive;
+		_slices [_currentSlice].r = r;
+		_slices [_currentSlice].g = g;
+		_slices [_currentSlice].b = b;
+	}
+
+
+	public void ClearSlices()
+	{
+		_slices [0].IsAlive = false;
+		_slices [1].IsAlive = false;
+		SetLifeColor (0f, 0f, 0f, 0.25f);
+	}
+
+	public void SetAlive()
+	{
+		_currentSlice = 0;
+
+		_slices [_currentSlice].IsAlive = true;
+		SetLifeColor (1f, 1f, 1f, 1f);
+	}
+		
+
+	public bool GetCurrentSliceIsAlive()
+	{
+		return ( _slices [_currentSlice].IsAlive );
+	}
+
+
+	public void SetOffScreenSliceState(bool state)
+	{
+		int offScreenSlice = 0;
+		if (_currentSlice == 0)
+			offScreenSlice = 1;
+
+		_slices [offScreenSlice].IsAlive = state;
+	}
+
+		
+	public void FlipSlices()
+	{
+		ToggleCurrentSlice ();
+
+		if (_slices [_currentSlice].IsAlive) {
+			SetLifeColor (1f, 1f, 1f, 1f);
+		} else {
+			SetLifeColor (0f, 0f, 0f, 0.25f);
+		}
+	}
+
+
+	public void SetLifeColor (float r, float g, float b, float a) 
+	{
+		lifeSprite.GetComponent<Renderer> ().material.color = new Color (r, g, b,  a);
+	}
+
+
+}
---
+++ Assets/Scripts/GridObj.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class GridObj : MonoBehaviour {
+
+
+	public List<List<GameObject>> GridCells { get; set; } 
+
+	private int mCols = 1;
+	private int mRows = 1;
+
+	private float mStartX;
+	private float mStartY;
+	private float mDeltaX;
+	private float mDeltaY;
+
+
+	public static GridObj Instance;
+
+	void Awake () {
+		
+		Instance = this;
+
+	}
+		
+	// Use this for initialization
+	void Start () {
+
+		GridCells = new List<List<GameObject>> ();
+
+		SetCoorSys (-8.0f, -3.0f, 0.2f, 0.2f);
+		CreateGird (64, 32);
+
+		CreateRandomGrid (25);
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+
+		ProcessGrid ();
+		FlipGrid ();
+	}
+
+	public void SetCoorSys(float startX, float startY, float deltaX, float deltaY)
+	{
+		mStartX = startX;
+		mStartY = startY;
+		mDeltaX = deltaX;
+		mDeltaY = deltaY;
+
+	}
+
+	public void CreateGird(int cols, int rows)
+	{
+		mCols = cols;
+		mRows = rows;
+
+		float x = mStartX, y = mStartY, z = 1f;
+
+		for (int r = 0; r < mRows; r++) {
+
+			List<GameObject> RowCells = new List<GameObject> ();
+
+			x = mStartX;
+			for (int c = 0; c < mCols; c++) {
+
+				GameObject _cellObj = Instantiate (Resources.Load ("Prefabs/CellObj", typeof(GameObject))) as GameObject;
+				_cellObj.transform.position = new Vector3(x, y, z);
+
+				CellObj cellObjectScript = _cellObj.GetComponent<CellObj> ();
+				cellObjectScript.SetCurrentSliceData (false, 0f, 0f, 0f);
+				cellObjectScript.ClearSlices ();//some redundancey
+
+				RowCells.Add (_cellObj);
+
+				x += mDeltaX;
+			}
+
+			y += mDeltaY;
+
+			GridCells.Add (RowCells);
+		}
+		
+	}
+
+	public void ProcessGrid()
+	{
+		for (int r = 1; r < mRows - 4; r++) {
+
+			List<GameObject> rowA = GridCells [r];
+			List<GameObject> rowB = GridCells [r+1];
+			List<GameObject> rowC = GridCells [r+2];
+
+			for (int c = 1; c < mCols - 4; c++) {
+
+				int fitness = 0; 
+				fitness += GetCellLife (rowB [c - 1]);
+				fitness += GetCellLife (rowB [c + 1]);
+				fitness += GetCellLife (rowA [c - 1]);
+				fitness += GetCellLife (rowA [c]);
+				fitness += GetCellLife (rowA [c + 1]);
+				fitness += GetCellLife (rowC [c - 1]);
+				fitness += GetCellLife (rowC [c]);
+				fitness += GetCellLife (rowC [c + 1]);
+
+				ProcessRule (rowB [c], fitness);
+			}
+		}
+	}
+
+	private int GetCellLife(GameObject _cellObj)
+	{
+		CellObj cellObjectScript = _cellObj.GetComponent<CellObj> ();
+		if (cellObjectScript.GetCurrentSliceIsAlive ())
+			return 1;
+		else
+			return 0;
+	}
+
+
+	//public bool GetRuleResult(int state, int fitness)
+	private void ProcessRule(GameObject _cellObj, int fitness)
+	{
+		bool updatedState;
+		CellObj cellObjectScript = _cellObj.GetComponent<CellObj> ();
+
+		bool isAlive = cellObjectScript.GetCurrentSliceIsAlive ();
+
+		if (isAlive == false && fitness == 3)
+			updatedState = true;
+		else if (isAlive == true && (fitness >= 2 || fitness <= 3))
+			updatedState = true;
+		else
+			updatedState = false;
+
+		cellObjectScript.SetOffScreenSliceState (updatedState);
+	}
+
+
+
+	public void FlipGrid()
+	{
+		for (int r = 0; r < mRows; r++) {
+
+			List<GameObject> rowCells = GridCells [r];
+
+			for (int c = 0; c < mCols; c++) {
+
+				GameObject _cellObj = rowCells [c];
+				CellObj cellObjectScript = _cellObj.GetComponent<CellObj> ();
+
+				cellObjectScript.FlipSlices ();
+			}
+		}
+	}
+
+
+	public void CreateRandomGrid(int frequency)
+	{
+		for (int r = 0; r < mRows; r++) {
+
+			List<GameObject> rowCells = GridCells [r];
+
+			for (int c = 0; c < mCols; c++) {
+
+				int value = Random.Range (0, 100);
+				if (value < frequency) {
+					GameObject _cellObj = rowCells [c];
+					CellObj cellObjectScript = _cellObj.GetComponent<CellObj> ();
+
+
+					cellObjectScript.SetAlive ();
+				}
+			}
+		}
+	}
+
+		
+		
+}
